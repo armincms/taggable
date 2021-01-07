3f3e6d042e269566c32c5671b3548c10d18f211a
Ismail Zare <zarehesmaiel@gmail.com>
2021-01-07 13:04:20 +0330

    Removes junks

--- resources/js/tool.js
+++
@@ -1,9 +0,0 @@
-Nova.booting((Vue, router, store) => {
-  router.addRoutes([
-    {
-      name: 'taggable',
-      path: '/taggable',
-      component: require('./components/Tool'),
-    },
-  ])
-})
